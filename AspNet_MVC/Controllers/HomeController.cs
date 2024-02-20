6e2b6aed4c628c2c812b8acdd1d0e1192f4467da
Marko Misimovic <misimovicmarko@gmail.com>
2024-02-20 11:53:53 +0100

    Add project files.

---
+++ AspNet_MVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewData["Title"] = "Home";
+			return View();
+		}
+	}
+}
