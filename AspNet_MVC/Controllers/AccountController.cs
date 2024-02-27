f7d5ccb5b51fb371f8e6d3d48d30021ae5373acd
Marko Misimovic <misimovicmarko@gmail.com>
2024-02-27 16:10:14 +0100

    Account page added

---
+++ AspNet_MVC/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_MVC.Controllers
+{
+	public class AccountController : Controller
+	{
+		[Route ("/account/details")]
+		public IActionResult Account()
+		{
+			return View();
+		}
+	}
+}
